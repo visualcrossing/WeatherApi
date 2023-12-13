a6496c37a3b0b8a026c95d1141dc01ce448d53ee
Visual Crossing Corporation <52202145+visualcrossing@users.noreply.github.com>
2023-12-13 17:33:08 -0500

    Update WeatherForecastRetrieval.java

--- Java/com/visualcrossing/weather/samples/WeatherForecastRetrieval.java
+++ Java/com/visualcrossing/weather/samples/WeatherForecastRetrieval.java
@@ -60,7 +60,7 @@ public class WeatherForecastRetrieval {
 			.setParameter("contentType", "json")
 			.setParameter("unitGroup", "metric")
 			.setParameter("locationMode", "single")
-			.setParameter("key", "1PYNQ6AWUDJE9AFERDCHJHSXK")
+			.setParameter("key", "YOUR_API_KEY")
 			.setParameter("locations", "London,UK");
 
 		HttpGet get = new HttpGet(builder.build());
